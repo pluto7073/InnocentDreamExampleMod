b5fe65092bd597db6269181a330a5a8d8d1b904b
pluto7073 <20planetpluto05@gmail.com>
2023-03-01 20:28:44 -0500

    lt-2023.0.0

---
+++ src/main/java/io/innocentdream/example/ExampleMod.java
@@ -0,0 +1,10 @@
+package io.innocentdream.example;
+
+import io.innocentdream.mods.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello World");
+    }
+}
